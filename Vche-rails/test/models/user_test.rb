edbad50650a333e383549a704d765af3d9c174e5
kaikoga <info@mail.kaikoga.net>
2021-07-20 22:36:32 +0900

    rake db:migrate

--- Vche-rails/test/models/user_test.rb
+++ Vche-rails/test/models/user_test.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                           :bigint           not null, primary key
+#  crypted_password             :string(255)
+#  email                        :string(255)      not null
+#  last_activity_at             :datetime
+#  last_login_at                :datetime
+#  last_login_from_ip_address   :string(255)
+#  last_logout_at               :datetime
+#  remember_me_token            :string(255)
+#  remember_me_token_expires_at :datetime
+#  salt                         :string(255)
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                                (email) UNIQUE
+#  index_users_on_last_logout_at_and_last_activity_at  (last_logout_at,last_activity_at)
+#  index_users_on_remember_me_token                    (remember_me_token)
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
