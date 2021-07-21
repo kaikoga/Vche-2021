29550d84edd18d7d4a5fc0eb73567fe0bd7cfd63
kaikoga <info@mail.kaikoga.net>
2021-07-22 01:02:49 +0900

    throw away bootstrap

--- Vche-rails/app/javascript/packs/application_bootstrap.js
+++ Vche-rails/app/javascript/packs/application_bootstrap.js
@@ -9,7 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import 'bootstrap';
-import '../stylesheets/application';
+import '../stylesheets/application_bootstrap';
 
 Rails.start()
 Turbolinks.start()
