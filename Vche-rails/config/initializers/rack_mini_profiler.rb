63448cf9ca63f3bbda12a13f87803df2129a5b65
kaikoga <info@mail.kaikoga.net>
2021-07-23 19:28:02 +0900

    adjust header

---
+++ Vche-rails/config/initializers/rack_mini_profiler.rb
@@ -0,0 +1,5 @@
+# see https://github.com/MiniProfiler/rack-mini-profiler/blob/master/README.md
+
+Rack::MiniProfiler.config.position = 'bottom-left'
+Rack::MiniProfiler.config.start_hidden = false
+Rack::MiniProfiler.config.toggle_shortcut = 'P'
