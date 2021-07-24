fcc530c3b76456b036ccd7a0b4a9de4309d45d39
kaikoga <info@mail.kaikoga.net>
2021-07-24 22:07:09 +0900

    seed in transaction

--- Vche-rails/db/seeds.rb
+++ Vche-rails/db/seeds.rb
@@ -1,8 +1,9 @@
-['db/fixtures/*.rb', "db/fixtures/#{Rails.env}/*.rb"].
-  map { |path| File.join(Rails.root, path) }.
-  flat_map { |path| Dir.glob(path).sort }.
-  each do |seed|
-  puts seed
-  load seed
+ActiveRecord::Base.transaction do
+  ['db/fixtures/*.rb', "db/fixtures/#{Rails.env}/*.rb"].
+    map { |path| File.join(Rails.root, path) }.
+    flat_map { |path| Dir.glob(path).sort }.
+    each do |seed|
+    puts seed
+    load seed
+  end
 end
-
