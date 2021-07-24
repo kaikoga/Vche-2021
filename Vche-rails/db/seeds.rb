7f0d18371dddd37493a7b13db37105925a2baaf6
kaikoga <info@mail.kaikoga.net>
2021-07-24 16:55:36 +0900

    fix seed import order

--- Vche-rails/db/seeds.rb
+++ Vche-rails/db/seeds.rb
@@ -1,7 +1,6 @@
 ['db/fixtures/*.rb', "db/fixtures/#{Rails.env}/*.rb"].
   map { |path| File.join(Rails.root, path) }.
-  flat_map { |path| Dir.glob(path) }.
-  sort.
+  flat_map { |path| Dir.glob(path).sort }.
   each do |seed|
   puts seed
   load seed
