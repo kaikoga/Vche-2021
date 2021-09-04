174ff2a6026f7cc8a90b7a609370de2a572c3b6f
kaikoga <info@mail.kaikoga.net>
2021-09-05 06:16:38 +0900

    add link for edit staff

--- Vche-rails/app/loyalties/events/event_follows_loyalty.rb
+++ Vche-rails/app/loyalties/events/event_follows_loyalty.rb
@@ -2,4 +2,12 @@ class Events::EventFollowsLoyalty < ApplicationLoyalty
   def index?
     true
   end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    true
+  end
 end
--- Vche-rails/app/loyalties/events/event_histories/event_attendances_loyalty.rb
+++ Vche-rails/app/loyalties/events/event_histories/event_attendances_loyalty.rb
@@ -2,4 +2,12 @@ class Events::EventHistories::EventAttendancesLoyalty < ApplicationLoyalty
   def index?
     true
   end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    true
+  end
 end
