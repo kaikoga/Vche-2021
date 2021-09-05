8c1e67d5e3f6db150c8c515f5a796ff732707fa1
kaikoga <info@mail.kaikoga.net>
2021-09-05 17:38:14 +0900

    add irrelevant role

--- Vche-rails/app/models/concerns/enums/Role.rb
+++ Vche-rails/app/models/concerns/enums/Role.rb
@@ -3,16 +3,18 @@ module Enums::Role
 
   included do
     enumerize :role, in: [
+        :irrelevant,
         :owner,
         :instance_owner,
         :performer,
         :staff,
         :participant,
         :visitor,
-        :viewer
+        :viewer,
     ], default: :participant
 
     scope :owned, ->{ where(role: :owner) }
+    scope :active, ->{ where.not(role: :irrelevant) }
     scope :backstage_member, ->{ where(role: [:owner, :instance_owner, :performer, :staff]) }
     scope :audience, ->{ where(role: [:participant, :visitor, :viewer]) }
   end
