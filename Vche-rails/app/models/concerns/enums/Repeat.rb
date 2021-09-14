9a2e2769c0cea4ebcdcb759c3ac7447d1dea5d6b
kaikoga <info@mail.kaikoga.net>
2021-09-15 00:08:38 +0900

    resurrect one-shot event schedule

--- Vche-rails/app/models/concerns/enums/Repeat.rb
+++ Vche-rails/app/models/concerns/enums/Repeat.rb
@@ -3,15 +3,16 @@ module Enums::Repeat
 
   included do
     enumerize :repeat, in: [
-        :every_day,
-        :every_week,
-        :every_other_week,
-        :first_week,
-        :second_week,
-        :third_week,
-        :fourth_week,
-        :fifth_week,
-        :last_week
+      :oneshot,
+      :every_day,
+      :every_week,
+      :every_other_week,
+      :first_week,
+      :second_week,
+      :third_week,
+      :fourth_week,
+      :fifth_week,
+      :last_week
     ], default: :every_day
   end
 end
