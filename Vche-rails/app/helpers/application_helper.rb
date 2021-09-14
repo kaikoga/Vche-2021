f568c81df443a47692ffee66035346a461d5bf56
kaikoga <info@mail.kaikoga.net>
2021-09-14 23:48:03 +0900

    even better format repeat time spans

--- Vche-rails/app/helpers/application_helper.rb
+++ Vche-rails/app/helpers/application_helper.rb
@@ -7,13 +7,31 @@ module ApplicationHelper
   end
 
   def time_span_text(start_at, end_at)
+    repeat_time_span_text(:oneshot, start_at, end_at)[1..2]
+  end
+
+  def repeat_time_span_text(repeat, start_at, end_at)
+    case repeat.to_sym
+    when :every_day
+      repeat_text = EventSchedule.repeat.find_value(repeat).text
+      start_at_text = l(start_at, format: :hm)
+    when :every_other_week
+      repeat_text = EventSchedule.repeat.find_value(repeat).text
+      start_at_text = l(start_at)
+    when :every_week, :first_week, :second_week, :third_week, :fourth_week, :fifth_week, :last_week
+      repeat_text = "#{EventSchedule.repeat.find_value(repeat).text} #{l(start_at, format: '%A')}"
+      start_at_text = l(start_at, format: :hm)
+    else
+      repeat_text = ''
+      start_at_text = l(start_at)
+    end
     case (end_at.beginning_of_day - start_at.beginning_of_day) / 1.day
     when 0
-      [l(start_at), l(end_at, format: :hm)]
+      [repeat_text, start_at_text, l(end_at, format: :hm)]
     when 1
-      [l(start_at), l(end_at.change(hour:0), format: :hm).sub("0", "#{end_at.hour + 24}")]
+      [repeat_text, start_at_text, l(end_at.change(hour:0), format: :hm).sub("0", "#{end_at.hour + 24}")]
     else
-      [l(start_at), l(end_at)]
+      [repeat_text, start_at_text, l(end_at)]
     end
   end
 end
