7ebd26a987579f32c31b96381fb51363c6e432b4
kaikoga <info@mail.kaikoga.net>
2021-09-14 21:42:32 +0900

    better format time spans

--- Vche-rails/app/helpers/application_helper.rb
+++ Vche-rails/app/helpers/application_helper.rb
@@ -5,4 +5,15 @@ module ApplicationHelper
     else '48px'
     end
   end
+
+  def time_span_text(start_at, end_at)
+    case (end_at.beginning_of_day - start_at.beginning_of_day) / 1.day
+    when 0
+      [l(start_at), l(end_at, format: :hm)]
+    when 1
+      [l(start_at), l(end_at.change(hour:0), format: :hm).sub("0", "#{end_at.hour + 24}")]
+    else
+      [l(start_at), l(end_at)]
+    end
+  end
 end
