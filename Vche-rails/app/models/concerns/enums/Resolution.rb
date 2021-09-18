fe1723edc6609e84c1db72401ef71e6e161452eb
kaikoga <info@mail.kaikoga.net>
2021-09-19 08:30:49 +0900

    prettify resolutions with emoji

--- Vche-rails/app/models/concerns/enums/Resolution.rb
+++ Vche-rails/app/models/concerns/enums/Resolution.rb
@@ -3,12 +3,45 @@ module Enums::Resolution
 
   included do
     enumerize :resolution, in: [
-        :scheduled,
-        :announced,
-        :moved,
-        :canceled,
-        :ended,
-        :completed
+      :scheduled,
+      :announced,
+      :moved,
+      :canceled,
+      :ended,
+      :completed
     ], default: :scheduled
+
+    def resolution_emoji
+      Enums::Resolution.resolution_emoji(resolution)
+    end
+
+    def resolution_emoji_text
+      resolution_emoji + resolution_text
+    end
+
+    def resolution.emoji_options(**kwargs)
+      options(kwargs).map { |name, value| ["#{Enums::Resolution.resolution_emoji(value)}#{name}", value] }
+    end
   end
+
+  def resolution_emoji(resolution)
+    case resolution.to_sym
+    when :scheduled
+      '🗒'
+    when :announced
+      '▶️'
+    when :moved
+      '⚠️'
+    when :canceled
+      '🛑️'
+    when :ended
+      '✅'
+    when :completed
+      '📗'
+    else
+      '❔'
+    end
+  end
+
+  module_function :resolution_emoji
 end
