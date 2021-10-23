347a3a8ac09dd5299f3c22a4778f2fe1f13825ea
kaikoga <info@mail.kaikoga.net>
2021-10-23 16:01:27 +0900

    delete unused helper

--- Vche-rails/app/helpers/user_helper.rb
+++ Vche-rails/app/helpers/user_helper.rb
@@ -5,14 +5,4 @@ module UserHelper
 
     image_tag('/user_128x128.png', class: class_name, width: size_num, height: size_num, alt: user.display_name, title: user.display_name)
   end
-
-  # @deprecated
-  def user_twitter_id(user)
-    user.primary_sns_name
-  end
-
-  # @deprecated
-  def user_twitter_url(user)
-    user.primary_sns_url
-  end
 end
