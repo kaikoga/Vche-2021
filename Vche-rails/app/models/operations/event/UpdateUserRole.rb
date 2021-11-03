7abab4fed80c7e0d123075997e816a2d897de4ce
kaikoga <info@mail.kaikoga.net>
2021-11-04 00:16:41 +0900

    fix lonely operator

--- Vche-rails/app/models/operations/event/UpdateUserRole.rb
+++ Vche-rails/app/models/operations/event/UpdateUserRole.rb
@@ -27,6 +27,6 @@ class Operations::Event::UpdateUserRole < Operations::Operation
   attr_reader :event, :user, :role
 
   def current_role
-    user.event_follows.find_by(event: event)&.role.to_sym || :irrelevant
+    user.event_follows.find_by(event: event)&.role&.to_sym || :irrelevant
   end
 end
