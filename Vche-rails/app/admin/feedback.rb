e1142ee7e6f87f714e1c090cefdfb05bc5a390a0
kaikoga <info@mail.kaikoga.net>
2021-11-05 21:39:29 +0900

    resolve and unresolve feedbacks from admin

--- Vche-rails/app/admin/feedback.rb
+++ Vche-rails/app/admin/feedback.rb
@@ -27,4 +27,24 @@ ActiveAdmin.register Feedback do
     column :resolved_at
     actions
   end
+
+  member_action :resolve_now do
+    resource.update!(resolved_at: Time.current)
+    redirect_to admin_feedback_path(resource)
+  end
+
+  member_action :unresolve_now do
+    resource.update!(resolved_at: nil)
+    redirect_to admin_feedback_path(resource)
+  end
+
+  action_item :resolve_now, only: [:show, :edit] do
+    link_to 'Resolve now', resolve_now_admin_feedback_path(resource)
+  end
+
+  action_item :unresolve_now, only: [:show, :edit] do
+    link_to 'Unresolve now', unresolve_now_admin_feedback_path(resource)
+  end
+
+  permit_params :resolved_at
 end
