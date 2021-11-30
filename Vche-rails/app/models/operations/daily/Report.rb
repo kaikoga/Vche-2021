09f60dd346788c954aaddc295ece26ed131313dc
kaikoga <info@mail.kaikoga.net>
2021-11-30 09:39:22 +0900

    use incoming webhook with slack-notifier

--- Vche-rails/app/models/operations/daily/Report.rb
+++ Vche-rails/app/models/operations/daily/Report.rb
@@ -9,12 +9,16 @@ class Operations::Daily::Report < Operations::Operation
   def perform
     yesterday = 1.days.ago.beginning_of_day..1.days.ago.end_of_day
     message = []
+    message << "Environment: #{Vche.env}"
     message << "User: #{User.count} (+#{User.where(created_at: yesterday).count})"
     message << "Event: #{Event.count} (+#{Event.where(created_at: yesterday).count})"
     message << "EventFollow: #{EventFollow.count} (+#{EventFollow.where(created_at: yesterday).count})"
     message << "EventAttendance: #{EventAttendance.count} (+#{EventAttendance.where(created_at: yesterday).count})"
     message << "Feedback: #{Feedback.count} (+#{Feedback.where(created_at: yesterday).count})"
 
-    puts message.join("\n")
+    body = message.join("\n")
+    puts body
+    webhook = Rails.application.config.x.vche.slack_daily_report_webhook.presence
+    Slack::Notifier.new(webhook).ping(body) if webhook
   end
 end
