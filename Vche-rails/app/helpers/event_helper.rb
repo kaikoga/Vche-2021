9dd3c6eae851cf7968c58a735a63e4cb846342b0
kaikoga <info@mail.kaikoga.net>
2021-12-06 06:43:00 +0900

    rubocop --hand-correct --only Lint/UnreachableCode

--- Vche-rails/app/helpers/event_helper.rb
+++ Vche-rails/app/helpers/event_helper.rb
@@ -1,11 +1,11 @@
 module EventHelper
   def event_icon_tag(event, size: :medium)
-    return tag.span
+    # class_name = size ? "event__icon -#{size}" : 'event__icon'
+    # size_num = icon_size_num(size)
 
-    class_name = size ? "event__icon -#{size}" : 'event__icon'
-    size_num = icon_size_num(size)
+    # image_tag('/event_128x128.png', class: class_name, width: size_num, height: size_num, alt: event.name, title: event.fullname)
 
-    image_tag('/event_128x128.png', class: class_name, width: size_num, height: size_num, alt: event.name, title: event.fullname)
+    tag.span
   end
 
   def capacity_and_follow_text(event)
